fc04327301e7d9bcaba319610ebe948e2792ab78
sam <sam@freighttrust.com>
2021-01-22 07:08:05 -0800

    fix(toc): table of contents

--- generate.sh
+++ generate.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-asciidoctor -a stylesheet=adoc-github.css README.adoc
+asciidoctor -a stylesheet=adoc-github.css OMNIBUS.adoc
+rm docs/index.html
+mv OMNIBUS.html docs/index.html
+echo "==> Webage generated "

4fb1ce58382edff80e0be75681bc761e4ed2c56c
sam <sam@freighttrust.com>
2021-01-22 13:34:24 -0800

    spec 0.6.0

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-asciidoctor -a stylesheet=adoc-github.css OMNIBUS.adoc
+asciidoctor -a stylesheet=adoc-github.css specification.adoc
 rm docs/index.html
-mv OMNIBUS.html docs/index.html
+mv specification.html docs/index.html
 echo "==> Webage generated "
